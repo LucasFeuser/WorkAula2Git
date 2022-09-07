cc82bcc23109c130256252b4d8bef121f052a5c9
Lucas H S V Feuser <109012065+LucasFeuser@users.noreply.github.com>
2022-09-07 19:14:25 -0300

    Versionamento work

---
+++ Aula_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace aula_02
+{
+    internal class Program
+    {
+        //Classes -------------------------------------------------------
+
+        public class Criacao_Mapa{
+            public string[,] mapa = new string[10,10]; //Criando o mapa
+
+            public Criacao_Mapa(){
+                for(int i = 0; i < 10; i++){
+                    for(int j = 0; j < 10; j++){
+                        mapa[i,j] = "--";
+                    }
+                }
+            }
+        }
+    
+    public static void printa_tudo(string coisa){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                Console.WriteLine(coisa[i,j]);
+            }
+        }
+    }
+        //Chamando tudo -------------------------------------------------------
+    public static void Main()
+        {
+            var jogo = new Criacao_Mapa();
+                          
+            printa_tudo(jogo.mapa);
+
+        }  
+    }      
+}
